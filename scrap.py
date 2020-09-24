ebf048705990072a5028e0922d3561353c0ba938
R00t-h4x0r <rangga@localhost.localdomain>
2020-09-24 13:00:58 +0700

    2

--- scrap.py
+++ scrap.py
@@ -5,7 +5,7 @@ import time
 
 name = ['sergeeyxglpeabc1234','abakarkadifodpu2abc1234','mukhamed-alizjruabc1234','kdavdiyalaspabc1234','kamilahxvvasabc1234','mukhamedcktabc1234','rafikyibrabc1234','yusunpgujwabc1234','ismatmzu2lepabc1234','kholikbuckabc1234']
 email = ['sergeeyxglpeabc1234@mail.ru','abakarkadifodpu2abc1234@mail.ru','mukhamed-alizjruabc1234@mail.ru','kdavdiyalaspabc1234@mail.ru','kamilahxvvasabc1234@mail.ru','mukhamedcktabc1234@mail.ru','rafikyibrabc1234@mail.ru','yusunpgujwabc1234@mail.ru','ismatmzu2lepabc1234@mail.ru','kholikbuckabc1234@mail.ru']
-password = ['cja3u1Aqdj','aghXus3984','L2Neln07l','Ji7fxD1X','b7OBrt9o7','v90rbz3fW5','OBx81o4l','puJQijs0','m580cO8ato','VYnYs0mc22']
+password = ['scja3u1Aqdj','aghXus3984','L2Neln07l','Ji7fxD1X','b7OBrt9o7','v90rbz3fW5','OBx81o4l','puJQijs0','m580cO8ato','VYnYs0mc22']
 
 username = '//*[@id="inputUserName"]'
 mail = '//*[@id="inputEmail"]'
